909da312d43093d515480b82d427a517fef56f66
Batuhan Saç <batuhansac53@gmail.com>
2026-06-16 21:09:28 +0300

    feat: add project domain entities

---
+++ src/FreelanceOps.Domain/Projects/ProjectStatus.cs
@@ -0,0 +1,10 @@
+namespace FreelanceOps.Domain.Projects;
+
+public enum ProjectStatus
+{
+    Draft = 1,
+    Active = 2,
+    OnHold = 3,
+    Completed = 4,
+    Cancelled = 5
+}
---
+++ src/FreelanceOps.Domain/Projects/ProjectTaskPriority.cs
@@ -0,0 +1,9 @@
+namespace FreelanceOps.Domain.Projects;
+
+public enum ProjectTaskPriority
+{
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4
+}
